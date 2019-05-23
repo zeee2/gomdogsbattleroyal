ec16273aef838c8aae5c3d7e76f85549220d7b90
Hoshikuz <lu_shariel@naver.com>
2019-05-23 16:04:06 +0900

    Add files via upload

---
+++ Modules/Misc.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Net;
+using Newtonsoft.Json;
+using CSharpOsu;
+using CSharpOsu.Module;
+using CSharpOsu.Util;
+
+namespace GBR_Bot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        OsuClient _client = Program._osuclient;
+
+        [Command("!count")]
+        public async Task Plist()
+        {
+            string[] gbr = File.ReadAllLines("Resources/gbr.json");
+            string[] gbrc = File.ReadAllLines("Resources/gbrc.json");
+
+            await Context.Channel.SendMessageAsync($"GBR : {gbr.Length - 2}\nGBRC : {gbrc.Length - 2}");
+        }
+
+        [Command("!참가신청")]
+        public async Task Participate([Remainder] string command = null)
+        {
+            OsuUser[] Player = _client.GetUser(command);
+            OsuUser player = Player[0];
+
+            if (player == null)
+            {
+                await Context.Channel.SendMessageAsync("닉네임을 입력해주세요!");
+            }
+            else
+            {
+                Dictionary<string, string> pairs = new Dictionary<string, string>();
+                pairs.Add(Context.User.Username.ToString(), player.pp_rank.ToString());
+                string data = JsonConvert.SerializeObject(pairs, Formatting.Indented);
+
+                if (player.pp_rank < 30000)
+                {
+                    string json = "Resources/gbr.json";
+                    File.WriteAllText(json, data);
+                    var user = Context.User;
+                    var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "GBR PLAYER");
+                    await Context.Channel.SendMessageAsync("GBR 신청이 완료되었습니다!");
+                    await (user as IGuildUser).AddRoleAsync(role);
+                }
+                else
+                {
+                    string json = "Resources/gbrc.json";
+                    File.WriteAllText(json, data);
+                    var user = Context.User;
+                    var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "GBRC PLAYER");
+                    await Context.Channel.SendMessageAsync("GBRC 신청이 완료되었습니다!");
+                    await (user as IGuildUser).AddRoleAsync(role);
+                }
+            }
+        }
+
+        [Command("!add")]
+        public async Task Add([Remainder] string command = null)
+        {
+            if (command == null)
+            {
+                await Context.Channel.SendMessageAsync("닉네임을 입력해주세요!");
+                return;
+            }
+
+            OsuUser[] Player = _client.GetUser(command);
+            OsuUser player = Player[0];
+            Dictionary<string, string> pairs;
+            
+            if (player.pp_rank < 30000)
+            {
+                string json = File.ReadAllText("Resources/gbr.json");
+                var data = JsonConvert.DeserializeObject<dynamic>(json);
+
+                pairs = data.ToObject<Dictionary<string, string>>();
+                pairs.Add(player.username, player.pp_rank.ToString());
+                string datas = JsonConvert.SerializeObject(pairs, Formatting.Indented);
+                File.WriteAllText("Resources/gbr.json", datas);
+
+                await Context.Channel.SendMessageAsync("GBR에 추가되었습니다.");
+            }
+            else
+            {
+                string json = File.ReadAllText("Resources/gbrc.json");
+                var data = JsonConvert.DeserializeObject<dynamic>(json);
+
+                pairs = data.ToObject<Dictionary<string, string>>();
+                pairs.Add(player.username, player.pp_rank.ToString());
+                string datas = JsonConvert.SerializeObject(pairs, Formatting.Indented);
+                File.WriteAllText("Resources/gbrc.json", datas);
+
+                await Context.Channel.SendMessageAsync("GBRC에 추가되었습니다.");
+            }
+        }
+        
+
+        [Command("!닉네임")]
+        public async Task Nickname([Remainder]string comm)
+        {
+            Dictionary<string, string> pairs = new Dictionary<string, string>();
+            string json = "Resources/nickname.json";
+            pairs.Add(Context.User.Id.ToString(), comm);
+            string data = JsonConvert.SerializeObject(pairs, Formatting.Indented);
+            File.WriteAllText(json, data);
+
+            await Context.Channel.SendMessageAsync($"{Context.User.Username}님의 osu! 닉네임이 **{comm}**으로 변경되었습니다.");
+
+        }
+
+        [Command("!c")]
+        public async Task Calculator([Remainder]string mes)
+        {
+            string[] spl = mes.Split();
+            float score = float.Parse(spl[1]);
+
+            OsuUser[] Player = _client.GetUser(spl[0]);
+            OsuUser player = Player[0];
+
+            if (player.pp_rank < 10)
+                score *= 0.8f;
+            else if (player.pp_rank < 30)
+                score *= 0.81f;
+            else if (player.pp_rank < 60)
+                score *= 0.82f;
+            else if (player.pp_rank < 100)
+                score *= 0.825f;
+            else if (player.pp_rank < 200)
+                score *= 0.83f;
+            else if (player.pp_rank < 400)
+                score *= 0.835f;
+            else if (player.pp_rank < 700)
+                score *= 0.85f;
+            else if (player.pp_rank < 1000)
+                score *= 0.88f;
+            else if (player.pp_rank < 2000)
+                score *= 0.905f;
+            else if (player.pp_rank < 4000)
+                score *= 0.935f;
+            else if (player.pp_rank < 7000)
+                score *= 0.96f;
+            else if (player.pp_rank < 10000)
+            { }
+            else if (player.pp_rank < 15000)
+                score *= 1.125f;
+            else if (player.pp_rank < 20000)
+                score *= 1.25f;
+            else if (player.pp_rank < 25000)
+                score *= 1.375f;
+            else if (player.pp_rank < 30000)
+                score *= 1.5f;
+            else
+            { }
+
+            await Context.Channel.SendMessageAsync(score.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord;
+using Discord.Commands;
+using CSharpOsu;
+using CSharpOsu.Module;
+
+namespace GBR_Bot
+{
+    class Program
+    {
+        DiscordSocketClient _client;
+        CommandHandler _handler;
+        public static OsuClient _osuclient = new OsuClient("c7e4c336af96d85c90a76705a077bd5355b88b96");
+
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
+        {
+            if (Config.bot.token == "" || Config.bot.token == null)
+                return;
+            _client = new DiscordSocketClient(new DiscordSocketConfig { LogLevel = LogSeverity.Verbose });
+            _client.Log += Log;
+            _client.MessageReceived += MessageReceived;
+            await _client.LoginAsync(TokenType.Bot, Config.bot.token);
+            await _client.StartAsync();
+            _handler = new CommandHandler();
+            await _handler.InitializeAsync(_client);
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content.Contains("https://osu.ppy.sh/u"))
+            {
+                string mes = message.ToString();
+                string[] username = mes.Split(new string[] { "sh/" }, StringSplitOptions.None);
+                string[] username2 = username[1].Split('/');
+                OsuUser[] Player = _osuclient.GetUser(username2[1]);
+                OsuUser player = Player[0];
+
+                if (player == null)
+                {
+                    await message.Channel.SendMessageAsync("올바른 유저페이지를 입력해주세요.");
+                }
+                else if (player.pp_rank < 30000)
+                {
+                    await message.Channel.SendMessageAsync($"**{player.username}** 님의 랭킹은 **#{player.pp_rank}**이며, GBR에 참가하실 수 있습니다.");
+
+                    if (player.pp_rank < 10)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.2**로, 최종 배율은 **x0.8**으로 계산됩니다.");
+                    else if (player.pp_rank < 30)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.19**로, 최종 배율은 **x0.81**로 계산됩니다.");
+                    else if (player.pp_rank < 60)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.18**으로, 최종 배율은 **x0.82**로 계산됩니다.");
+                    else if (player.pp_rank < 100)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.175**로, 최종 배율은 **x0.825**로 계산됩니다.");
+                    else if (player.pp_rank < 200)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.17**으로, 최종 배율은 **x0.83**으로 계산됩니다.");
+                    else if (player.pp_rank < 400)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.165**로, 최종 배율은 **x0.835**로 계산됩니다.");
+                    else if (player.pp_rank < 700)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.15**로, 최종 배율은 **x0.85**로 계산됩니다.");
+                    else if (player.pp_rank < 1000)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.12**로, 최종 배율은 **x0.88**으로 계산됩니다.");
+                    else if (player.pp_rank < 2000)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.095**로, 최종 배율은 **x0.905**로 계산됩니다.");
+                    else if (player.pp_rank < 4000)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.065**로, 최종 배율은 **x0.935**로 계산됩니다.");
+                    else if (player.pp_rank < 7000)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티는 **-0.04**로, 최종 배율은 **x0.96**로 계산됩니다.");
+                    else if (player.pp_rank < 10000)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 패널티나 베네핏이 없습니다. 최종 배율은 **x1.00**으로 계산됩니다.");
+                    else if (player.pp_rank < 15000)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 베네핏은 **0.125**로, 최종 배율은 **x1.125**로 계산됩니다.");
+                    else if (player.pp_rank < 20000)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 베네핏은 **0.25**로, 최종 배율은 **x1.25**로 계산됩니다.");
+                    else if (player.pp_rank < 25000)
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 베네핏은 **0.375**로, 최종 배율은 **x1.375**로 계산됩니다.");
+                    else
+                        await message.Channel.SendMessageAsync("랭킹에 해당하는 베네핏은 **0.5**로, 최종 배율은 **x1.5**로 계산됩니다.");
+                }
+                else
+                {
+                    await message.Channel.SendMessageAsync($"**{player.username}** 님의 랭킹은 **#{player.pp_rank}**이며, GBRC에 참가하실 수 있습니다.");
+                }
+            }
+            else return;
+        }
+
+        private async Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.Message);
+        }
+    }
+}
